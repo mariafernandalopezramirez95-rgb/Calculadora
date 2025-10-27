4ddcdb2b22f4fe286c4d0c034552c29759660156
mariafernandalopezramirez95-rgb <mariafernandalopezramirez95@gmail.com>
2025-10-27 18:53:49 +0100

    Fix turso-test endpoint to use ESM import

--- api/turso-test.js
+++ api/turso-test.js
@@ -1,15 +1,14 @@
-const { createClient } = require('@libsql/client');
+import { createClient } from '@libsql/client';
 
-module.exports = async function (req, res) {
+export default async function handler(req, res) {
   const client = createClient({
     url: process.env.TURSO_DATABASE_URL,
     authToken: process.env.TURSO_AUTH_TOKEN,
   });
-
   try {
     const result = await client.execute('SELECT 1 as connected;');
     return res.status(200).json({ connected: true, result });
   } catch (err) {
     return res.status(500).json({ connected: false, error: err.message });
   }
-};
+}
